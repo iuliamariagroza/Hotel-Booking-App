2c380d2abd721faac116031eb06db19070dfc25e
iuliamariagroza <groza_iuliabv@yahoo.com>
2024-09-30 20:03:43 +0300

    added e2e tests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,6 +19,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -1,18 +1,47 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const URI_URL = "http://localhost:5173/";
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test('should allow the user to sign in', async ({ page }) => {
+  await page.goto(URI_URL);
+
+  await page.getByRole("link", {name: "Sign In"}).click();
+
+  await expect(page.getByRole("heading", {name: "Sign In"})).toBeVisible();
+
+  await page.locator("[name=email]").fill("1@1.com");
+  await page.locator("[name=password]").fill("password");
+
+  await page.getByRole("button", {name: "Login"}).click(); 
+
+  await expect(page.getByText("Sign In Successful!")).toBeVisible();
+  await expect(page.getByRole("link", {name: "My Bookings"})).toBeVisible();
+  await expect(page.getByRole("link", {name: "My Hotels"})).toBeVisible();
+  await expect(page.getByRole("button", {name: "Sign Out"})).toBeVisible();
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+test('should allow the user to register', async ({ page }) => {
+  const testEmail = `test_register_${Math.floor(Math.random() * 90000) +10000 }@test.com`
+  await page.goto(URI_URL);
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.getByRole("link", {name: "Sign In"}).click();
+  await page.getByRole("link", {name: "Create an account here"}).click();
+
+  await expect(page.getByRole("heading", {name: "Create An Account"})).toBeVisible();
+
+  await page.locator("[name=firstName]").fill("test_firstName");
+  await page.locator("[name=lastName]").fill("test_lastName");
+  await page.locator("[name=email]").fill(testEmail);
+  await page.locator("[name=password]").fill("password123");
+  await page.locator("[name=confirmPassword ]").fill("password123");
+
+  await page.getByRole("button", {name: "Create Account"}).click();
+
+  await expect(page.getByText("Registration Success!")).toBeVisible();
+  await expect(page.getByRole("link", {name: "My Bookings"})).toBeVisible();
+  await expect(page.getByRole("link", {name: "My Hotels"})).toBeVisible();
+  await expect(page.getByRole("button", {name: "Sign Out"})).toBeVisible();
+
 });
+
